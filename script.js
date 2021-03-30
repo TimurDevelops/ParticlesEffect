ab81d7156c1bae26417bc66534541ec90ff79fd8
TimurDevelops <timur.eremchenko@yandex.ru>
2021-03-30 16:17:44 +0300

    Particles Effect ready

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let array = [];
+let particles = [];
 
 // noinspection JSUnusedGlobalSymbols
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
-  // p = new Particle();
-  array.push(new Particle());
+  const particlesLength = Math.floor(window.innerWidth / 10);
+  for (let i = 0; i < particlesLength; i++) {
+    particles.push(new Particle());
+  }
 }
 
 // noinspection JSUnusedGlobalSymbols
 function draw() {
   background(55, 100, 144)
-  array.forEach(point => {
-    point.draw();
-    point.update();
+  particles.forEach((particle, index) => {
+    particle.draw();
+    particle.update();
+    particle.checkParticles(particles.slice(index));
   })
-  // fill(0)
-  if(mouseIsPressed){
-    array.push(new Particle(mouseX, mouseY));
+  if (mouseIsPressed) {
+    particles.push(new Particle(mouseX, mouseY));
   }
 }
 
@@ -25,7 +27,7 @@ class Particle {
   constructor(x, y) {
 
     // Position
-    if(x === undefined && y === undefined){
+    if (x === undefined && y === undefined) {
       this.pos = createVector(random(width), random(height))
     } else {
       this.pos = createVector(x, y)
@@ -48,12 +50,23 @@ class Particle {
     circle(this.pos.x, this.pos.y, this.size);
   }
 
-  edges(){
-    if (this.pos.y < 5 || this.pos.y > height - 5){
+  edges() {
+    if (this.pos.y < 5 || this.pos.y > height - 5) {
       this.vel.y = -this.vel.y;
     }
-    if(this.pos.x < 5 || this.pos.x > width - 5){
+    if (this.pos.x < 5 || this.pos.x > width - 5) {
       this.vel.x = -this.vel.x;
     }
+  }
+
+  checkParticles(particles){
+    particles.forEach(particle => {
+      const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+      if(d< 120){
+        stroke('rgba(255,255,255,0.1)')
+        line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y)
+      }
+    })
+
   }
 }
